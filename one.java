892042ebe504c726da1f28e1471e80818077d91a
Mulamuleli Mammba <mammba001@gmail.com>
2025-02-05 09:28:29 +0200

    Commit 3

---
+++ one.java
@@ -0,0 +1,16 @@
+public class one{
+
+public static void main(String[] args){
+
+String holder = uno();
+System.out.println(holder);
+};
+
+public static String uno(){
+
+return "This is a test";
+
+};
+
+
+}
